eaddd6b4f1bd2c7ddc3a21085f90f412f55f64dd
Patricio Perez <patricioperezyolo@gmail.com>
2023-04-16 18:35:22 -0600

    Codigo/Commit Inicial

---
+++ archivo1.py
@@ -0,0 +1,11 @@
+class gato:
+    color_de_ojos = ""
+    color_de_pelaje = ""
+
+    def __init__(self) -> None:
+        print(f"Meow, soy un gato con ojos {self.color_de_ojos} y pelaje {self.color_de_pelaje}")
+
+Kiara = gato()
+Kiara.color_de_ojos="verdes"
+Kiara.color_de_pelaje="blanco, negro y cafe"
+Kiara.__init__()
